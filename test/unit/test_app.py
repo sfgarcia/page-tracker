338f7936e950c44669c2875b41d1897611b6e5ae
sfgarcia <sfgarcia@uc.cl>
2024-03-07 14:59:32 -0300

    Update app

--- test/unit/test_app.py
+++ test/unit/test_app.py
@@ -1,3 +1,5 @@
+import unittest.mock
+
 import pytest
 
 from src.page_tracker.app import app
@@ -5,3 +7,16 @@ from src.page_tracker.app import app
 @pytest.fixture
 def http_client():
     return app.test_client()
+
+@unittest.mock.patch("src.page_tracker.app.redis")
+def test_should_call_redis_incr(mock_redis, http_client):
+    # Given
+    mock_redis.return_value.incr.return_value = 5
+
+    # When
+    response = http_client.get("/")
+
+    # Then
+    assert response.status_code == 200
+    assert response.text == "This page has been seen 5 times."
+    mock_redis.return_value.incr.assert_called_once_with("page_views")
